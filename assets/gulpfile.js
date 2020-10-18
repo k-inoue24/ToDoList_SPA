91819c5aa6a4e817c930bc4a4303f950f9e7306e
k.inoue <>
2020-10-18 18:24:17 +0900

    TOPページ実装（暫定CSS）

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -2,7 +2,6 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sass = require("gulp-sass");
-var pug = require('gulp-pug');
 var browserSync = require('browser-sync');
 
 //sass
@@ -15,16 +14,6 @@ gulp.task("sass", function() {
         }))
         .pipe(plumber())
         .pipe(gulp.dest("../src/css"));
-});
-
-//pug
-gulp.task("pug", function() {
-    return gulp.src('pug/*.pug')
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest("../src"));
 });
 
 //browser sync
@@ -48,9 +37,8 @@ gulp.task('bs-reload', function(done) {
 //watch
 gulp.task('watch', function() {
     gulp.watch('sass/*.scss', gulp.task('sass'));
-    gulp.watch('pug/*.pug', gulp.task('pug'));
     gulp.watch('sass/*.scss', gulp.task('bs-reload'));
     gulp.watch('pug/*.pug', gulp.task('bs-reload'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'pug', 'watch', 'browser-sync'));
+gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync'));
--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,84 @@ var vm = new Vue({
     data: {
         newTask: '',
         newHour: '',
-        totalHour: 0,
-        todos: []
+        id: 1,
+        todos: [],
+    },
+    watch: {
+      todos: {
+          handler: function() {
+              localStorage.setItem('todos', JSON.stringify(this.todos));
+          },
+          deep: true
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
         addItem: function() {
             var taskItem = {
+                tasdDay: this.today,
+                taskId: this.lastId,
                 taskTitle: this.newTask,
                 taskHour: this.newHour,
                 taskDone: false
             }
-            this.totalHour += parseInt(this.newHour, 10);
-            this.todos.push(taskItem);
-            this.newTask = '';
-            this.newHour = '';
+            if(this.newTask != '' && this.newHour != '') {
+                this.todos.push(taskItem);
+                this.newTask = '';
+                this.newHour = '';
+            }
+        },
+        deleteItem: function(item) {
+          if (confirm('削除してよろしいでしょうか？')) {
+            this.todos = this.todos.filter(function(todo){
+              return todo.taskId != item.taskId;
+            },this)
+          }
         }
     },
     computed: {
+        today: function() {
+            var today = new Date();
+            var dayArray = new Array("日","月","火","水","木","金","土");
+            var todayString = today.getFullYear() + '年' + (today.getMonth() + 1) + '月' + today.getDate() + '日' + '（' + dayArray[today.getDay()] + '）'
+            return todayString;
+        },
+        remainingTask: function(){
+            return this.todos.filter(function(todo){
+                return todo.taskDone == false;
+            },this)
+        },
+        remainingHour: function(){
+            var remainingHour = 0;
+            this.remainingTask.forEach(function(el){
+                remainingHour += parseInt(el.taskHour, 10);
+            });
+            return remainingHour;
+        },
+        completedTask: function(){
+            return this.todos.filter(function(todo){
+                return todo.taskDone == true
+            },this)
+        },
+        completedHour: function(){
+            var completedHour = 0;
+            this.completedTask.forEach(function(el){
+                completedHour += parseInt(el.taskHour, 10);
+            });
+            return completedHour;
+        },
+        lastId: function() {
+          var lastId = 1;
+          this.todos.forEach(function(el){
+            if(lastId <= el.taskId) lastId = el.taskId + 1;
+          });
+          return lastId;
+        },
+        consoleChk: function(){
+            console.log(this.todos)
+        },
 
     }
 });
